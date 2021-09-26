a78b337c21e2c699851064af175df2122622aec4
atirson <fabiano.oliveira2213@gmail.com>
2021-09-26 19:35:57 -0300

    add - Timeline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header/Header';
+import { Timeline } from '../components/Timeline/Timeline';
 
 export default function Home() {
   return (
@@ -8,9 +9,10 @@ export default function Home() {
      justify="center"
      bg="black"
     >
-    <Flex flex="1" maxWidth={1480} flexDirection="row" h="100vh" >
+    <Flex flex="1" maxWidth={1480} flexDirection="row" h="100%" >
       <Box minW="60%" bg="white" height="100%">
         <Header />
+        <Timeline />
       </Box>
       <Box minW="40%" bg="backgroundRight" height="100%"></Box>
     </Flex>
